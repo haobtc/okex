4b57c1d5ad44baebbe90ca51333eb73d45de4634
winlin <pcliuguangtao@163.com>
2017-12-26 16:13:55 +0800

    remove key from test files

--- examples/basic.py
+++ examples/basic.py
@@ -13,5 +13,5 @@ print client.ticker(symbol)
 print client.trades(symbol)
 print client.depth(symbol)
 
-authClient = OkexTradeClient('ee8241b2-5af2-4eee-a057-3a5189d7f39b', '4811AA5AA4F60E7463AD52CBBF921C36', proxies=proxies)
+authClient = OkexTradeClient('', '', proxies=proxies)
 print authClient.balances()
